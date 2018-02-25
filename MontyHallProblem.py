bfe0e29c3bc94f38054cce8b14a1944772c64f0b
andrewklajman <andrew.klajman@gmail.com>
2018-02-26 09:37:19 +1100

    Add files via upload

--- MontyHallProblem.py
+++ MontyHallProblem.py
@@ -1,20 +1,54 @@
 import random
 
 
-prizeBehindDoor = random.randrange(1,4)
-doorToRemove = (random.randrange(1,3) == 1)
+def result():
+    DOORS = ['G', 'G', 'G']
+    DOORS[random.randrange(0,3)] = 'C'
+    ContestantChoice = random.randrange(0,3)
 
-doors = ['0','0','0']
-doors[prizeBehindDoor - 1] = '*'
+    HostChoice = 0
+    for i in range(3):
+        if i != ContestantChoice and DOORS[i] == 'G':
+            HostChoice = i
+            break
 
-for door in range(len(doors)):
-##    print(door)
-    if doors[door] == '*': continue
-    if doorToRemove:
-        doors[door] ='X'
-##        print(door)
-        break
-    doorToRemove = True
-    
 
-print(doors)
+    Result1 = DOORS[ContestantChoice] == 'C'
+
+    for i in range(3):
+        if i != ContestantChoice and i != HostChoice:
+            ContestantChoice = i
+            break
+
+    Result2 = DOORS[ContestantChoice] == 'C'
+    return(Result1, Result2)
+
+
+
+with open('output.csv','w') as f:
+    for i in range(1000000):
+        GameResult = result()
+        ouput = str(GameResult[0]) + ',' + str(GameResult[1]) + '\n'
+        f.write(ouput)
+        if i % 1000 ==0: print(i)
+
+
+
+##
+##prizeBehindDoor = random.randrange(1,4)
+##doorToRemove = (random.randrange(1,3) == 1)
+##
+##doors = ['0','0','0']
+##doors[prizeBehindDoor - 1] = '*'
+##
+##for door in range(len(doors)):
+####    print(door)
+##    if doors[door] == '*': continue
+##    if doorToRemove:
+##        doors[door] ='X'
+####        print(door)
+##        break
+##    doorToRemove = True
+##    
+##
+##print(doors)
